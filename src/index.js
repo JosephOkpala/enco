ca33a1f64fff6cc2304d65bef55da1161b582d65
JosephOkpala <okpalajoseph995@gmail.com>
2022-10-02 02:42:21 +0100

    deleted files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './App.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
-import { getCLS, getFID, getLCP } from 'web-vitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +12,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-console.log(getCLS);
-console.log(getFID);
-console.log(getLCP);
